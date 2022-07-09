ee81627703ac7dbb09b3e5c81b6c709efbfdc5cc
Mushtari Sadia <midnight.scream98@gmail.com>
2022-07-09 19:36:21 +0600

    updated page action

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Followed [this](https://github.com/gopinav/Chrome-Extensions/tree/master/HelloWorld) to create extension. Other relevant links :
 - https://betterprogramming.pub/how-to-create-and-publish-a-chrome-extension-in-20-minutes-6dc8395d7153
 - https://www.youtube.com/playlist?list=PLC3y8-rFHvwg2-q6Kvw3Tl_4xhxtIaNlY
+- https://stackoverflow.com/questions/58733672/chrome-extension-unhide-popup-text-on-url-string-match
--- popup.js
+++ popup.js
@@ -1,5 +1,41 @@
-$(function(){
-    $('#name').keyup(function(){
-        $('#greet').text('Hello ' + $('#name').val())
-    })
+
+const ids = [
+    "Test1",
+    "Test2",
+    "Test3"
+]
+ids.forEach(element => {
+    document.getElementById(element).style.display = 'none';
 });
+
+  chrome.tabs.query({active: true, currentWindow: true}, ([tab]) => {
+    if(tab.url.includes("dl.acm.org/doi"))
+    {
+        ids.forEach(element => {
+            document.getElementById(element).style.display = 'block';
+        });
+    }
+    else
+    {
+        ids.forEach(element => {
+            document.getElementById(element).style.display = 'none';
+        });
+    }
+  });
+
+
+  function viewProjects(){
+    chrome.runtime.sendMessage(
+        //request type variable indicates what content user is requesting (project/collaborators ?)
+        {request_type: "project"},
+        function(response) {
+            result = response.farewell;
+            var ul = document.getElementById("project-list");
+            var li = document.createElement("li");
+            li.setAttribute('id',result);
+            li.appendChild(document.createTextNode(result));
+            ul.appendChild(li);
+            
+        });
+    
+}

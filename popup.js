b1279d2cb0df437797834c0e6d6227994cf06761
Mushtari Sadia <midnight.scream98@gmail.com>
2022-07-09 19:36:21 +0600

    updated page action

--- popup.js
+++ popup.js
@@ -1,5 +1,24 @@
-$(function(){
-    $('#name').keyup(function(){
-        $('#greet').text('Hello ' + $('#name').val())
-    })
+
+const ids = [
+    "Test1",
+    "Test2",
+    "Test3"
+]
+ids.forEach(element => {
+    document.getElementById(element).style.display = 'none';
 });
+
+  chrome.tabs.query({active: true, currentWindow: true}, ([tab]) => {
+    if(tab.url.includes("dl.acm.org/doi"))
+    {
+        ids.forEach(element => {
+            document.getElementById(element).style.display = 'block';
+        });
+    }
+    else
+    {
+        ids.forEach(element => {
+            document.getElementById(element).style.display = 'none';
+        });
+    }
+  });

239b3b377c7d9d7ef4240cec41ede65f6429ebc1
Mushtari Sadia <midnight.scream98@gmail.com>
2022-07-12 03:43:06 +0600

    added initial api calls

--- background.js
+++ background.js
@@ -1,21 +1,52 @@
-var serverhost = 'http://127.0.0.1:8000';
+import { postData,serverhost,token } from './lib.js';
 
-	chrome.runtime.onMessage.addListener(
-		function(request, sender, sendResponse) {
+//Authorization
+
+// const data = { username: 'tahmeed', password: 'tahmeed' };
+// //POST request with body equal on data in JSON format
+// fetch(serverhost + '/auth/token/', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+//   body: JSON.stringify(data),
+// })
+// .then((response) => response.json())
+// //Then with the data from the response in JSON...
+// .then((data) => {
+// 	token = data.get('token');
+// 	console.log(token); // JSON data parsed by `data.json()` call
+// })
+// //Then with the error genereted...
+// .catch((error) => {
+//   console.error('Error:', error);
+// });
+
+  
+//   postData(serverhost + '/auth/token/', { username: 'tahmeed', password: 'tahmeed' })
+// 	.then(data => {
+// 	  token = data.get('token');
+// 	  console.log(token); // JSON data parsed by `data.json()` call
+// 	});
+
+
+
+	// chrome.runtime.onMessage.addListener(
+	// 	function(request, sender, sendResponse) {
 		  
 			  
-			var url = serverhost + '/wiki/get_wiki_summary/?topic='+ encodeURIComponent(request.topic) ;
+	// 		var url = serverhost + '/auth/token/' ;
 			
-			console.log(url);
+	// 		console.log(url);
 			
-			//var url = "http://127.0.0.1:8000/wiki/get_wiki_summary/?topic=%22COVID19%22"	
-			fetch(url)
-			.then(response => response.json())
-			.then(response => sendResponse({farewell: response}))
-			.catch(error => console.log(error))
+	// 		//var url = "http://127.0.0.1:8000/wiki/get_wiki_summary/?topic=%22COVID19%22"	
+	// 		fetch(url)
+	// 		.then(response => response.json())
+	// 		.then(response => sendResponse({farewell: response}))
+	// 		.catch(error => console.log(error))
 				
-			return true;  // Will respond asynchronously.
+	// 		return true;  // Will respond asynchronously.
 		  
-	});
+	// });
 
     
--- popup.js
+++ popup.js
@@ -1,15 +1,40 @@
+// import { postData,serverhost,token } from './lib.js';
+var serverhost = 'http://127.0.0.1:8000';
+var token='2a1f52e7dfab9ef3658b1263daa5dc3df5448eed'; 
+var headers =  {
+    'Content-Type': 'application/json',
+    'Authorization': 'Token ' + token
+}
+
+async function postData(url = '', data = {}) {
+	// Default options are marked with *
+	const response = await fetch(url, {
+	  method: 'POST', // *GET, POST, PUT, DELETE, etc.
+	  mode: 'cors', // no-cors, *cors, same-origin
+	  cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+	  credentials: 'same-origin', // include, *same-origin, omit
+	  headers: {
+		'Content-Type': 'application/json',
+		'Authorization': 'Token ' + token
+		// 'Content-Type': 'application/x-www-form-urlencoded',
+	  },
+	  redirect: 'follow', // manual, *follow, error
+	  referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+	  body: JSON.stringify(data) // body data type must match "Content-Type" header
+	});
+	return response.json(); // parses JSON response into native JavaScript objects
+  }
+
 
 const ids = [
     "saved",
     "Project",
     "Collaborator",
-    "Test3"
+    "Status"
 ]
 ids.forEach(element => {
     document.getElementById(element).style.display = 'none';
 });
-
-
 
 
 var currentTabUrl="";
@@ -19,9 +44,6 @@ if(tab.url.includes("dl.acm.org/doi"))
     currentTabUrl=tab.url;
     addPaperToProject("Unsorted",tab.url);
     document.getElementById("saved").style.display = 'block';
-    // ids.forEach(element => {
-    //     document.getElementById(element).style.display = 'block';
-    // });
     document.getElementById("Project").style.display = 'block';
 }
 else
@@ -32,29 +54,99 @@ else
 }
 });
 
-
-
 function viewProjects(){
-chrome.runtime.sendMessage(
-    //request type variable indicates what content user is requesting (project/collaborators ?)
-    {request_type: "project"},
-    function(response) {
-        result = response.farewell;
-        var ul = document.getElementById("project-list");
-        var li = document.createElement("li");
-        //result will be a list, add project names iteratively
-        li.setAttribute('id',result);
-        li.setAttribute("onclick",`addPaperToProject(${result},${currentTabUrl})`);
-        li.appendChild(document.createTextNode(result)); //addbutton with onclick, when clicked url will be added to project
-        ul.appendChild(li);
-        
-    });
+    fetch(serverhost + '/api/projects/', { headers })
+        .then(response => response.json())
+        .then(data => {
+            var ul = document.getElementById("project-list");
+            
+            data['results'].forEach(element => {
+                if(element['name']!="Unsorted")
+                {
+                    var button = document.createElement("button");
+                    button.setAttribute('id',element['name']);
+                    button.style.display = 'block';
+                    button.onclick = function () { addPaperToProject(element['name'],currentTabUrl,element['id']); };
+                    button.innerHTML = element['name'];
+                    ul.appendChild(button);
+                }
+            }); 
+        })
+}
+document.getElementById("view_projects_button").addEventListener("click", viewProjects);
+
+
+function viewCollaborators(projectID){
+    fetch(serverhost + '/api/projects/'+projectID+'/collaborators/', { headers })
+        .then(response => response.json())
+        .then(data => {
+            var ul = document.getElementById("collaborator-list");
+            
+            data.forEach(element => {
+                var button = document.createElement("button");
+                button.setAttribute('id',element['name']+element['id']);
+                button.style.display = 'block';
+                button.onclick = function () { assignCollaboratorToPaper(element['id'],projectID,currentTabUrl); };
+                button.innerHTML = element['username'];
+                ul.appendChild(button);
+            }); 
+        })
 }
 
-function addPaperToProject(projectName, paperUrl){
-    //send message to server with url of paper with project name
+function viewLists(projectID){
+    fetch(serverhost + '/api/projects/'+projectID+'/lists/', { headers })
+        .then(response => response.json())
+        .then(data => {
+            var ul = document.getElementById("status-list");
+            
+            data.forEach(element => {
+                var button = document.createElement("button");
+                button.setAttribute('id',element['name']+element['id']);
+                button.style.display = 'block';
+                button.onclick = function () { assignStatusToPaper(element['id'],projectID,currentTabUrl); };
+                button.innerHTML = element['name'];
+                ul.appendChild(button);
+            }); 
+        })
+}
+
+
+function addPaperToProject(projectName, paperUrl, projectID = 2){
+
+    var doi = paperUrl.substring(paperUrl.lastIndexOf("doi")+4);
+    
     if(projectName!="Unsorted")
     {
+        console.log(projectName);
         document.getElementById("Collaborator").style.display = 'block';
+        var vcb = document.getElementById("view_collaborators_button");
+        vcb.onclick = function () { viewCollaborators(projectID) };
+
+        document.getElementById("Status").style.display = 'block';
+        var vsb = document.getElementById("view_status_button");
+        vsb.onclick = function () { viewLists(projectID) };
+        //add api call for adding paper to project
+
+
     }
+    else
+    {
+        postData(serverhost + '/extension/add-paper/', { doi: doi})
+        .then(data => {
+        console.log(data); // JSON data parsed by `data.json()` call
+        });
+    }
+}
+
+
+function assignCollaboratorToPaper(collaboratorID,projectId,paperUrl)
+{
+    var doi = paperUrl.substring(paperUrl.lastIndexOf("doi")+4);
+    console.log("collaboratorID="+collaboratorID+" projectId="+projectId+" doi="+doi)
+}
+
+function assignStatusToPaper(statusID,projectId,paperUrl)
+{
+    var doi = paperUrl.substring(paperUrl.lastIndexOf("doi")+4);
+    console.log("statusID="+statusID+" projectId="+projectId+" doi="+doi)
 }
